bcda3111abe7aa230b935ed2deb882ab06c04cd1
neggwada <neggwada@tibidon>
2017-01-11 21:38:07 +0100

    test_verse_0

--- beer_song_test.rb
+++ beer_song_test.rb
@@ -33,7 +33,7 @@ class BeerSongTest < Minitest::Test
   end
 
   def test_verse_0
-    skip
+    #skip
     expected = "No more bottles of beer on the wall, no more bottles of beer.\n" \
       "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
     assert_equal expected, BeerSong.new.verse(0)
